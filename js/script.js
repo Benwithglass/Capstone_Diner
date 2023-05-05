624dbd4b867f811a2bf7ffa8dbf6cbb87e15ba70
Pham Tien Dat <tiendatpham2412@gmail.com>
2023-05-05 16:19:09 +0700

    xong dark mode - toggle dark mode se ko lam page tu dong load lai nua <3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
-document.getElementById("switchButton").onclick = function () {
-    document.getElementById("myBody").classList.toggle("dark");
+// document.getElementById("switchButton").onclick = function () {
+//     document.getElementById("myBody").classList.toggle("dark");
+// }
+
+// function myFunction() {
+//     var element = document.body;
+//     element.classList.toggle("dark");
+// }
+
+// toggle Dark mode thì page tự động reload dẫn đến page tự chuyển lại thành light mode nên phải thử cách dưới này...
+
+function darkMode () {
+    var setTheme = document.body;
+    
+    setTheme.classList.toggle("dark");
+    
+    var theme;
+
+    if(setTheme.classList.contains("dark")) {
+        console.log("dark mode");
+        theme = "DARK";
+    }else {
+        console.log("light mode");
+        theme = "LIGHT";
+    }
+
+    // save to localStorage
+
+    localStorage.setItem("pageTheme", JSON.stringify(theme));
+    // nhớ convert sang JSON... 
+}
+
+let getTheme = JSON.parse(localStorage.getItem("pageTheme"));
+console.log(getTheme);
+
+if (getTheme === "DARK"){
+    document.body.classList = "dark";
 }
